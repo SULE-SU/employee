6b3a4a7b0c26b51159779cbe53a6e006c34b86b8
SULE <sule1@oocl.com>
2025-09-09 15:36:26 +0800

    feat: getEmployeeById and add EmployeeNotFoundException

--- src/main/java/com/example/employee/EmployeeController.java
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("employee")
+@RequestMapping("/employees")
 public class EmployeeController {
     private List<Employee> employees = new ArrayList<>();
     private int id = 0;
@@ -17,8 +17,16 @@ public class EmployeeController {
     @ResponseStatus(HttpStatus.CREATED)
     public Employee createEmployee(@RequestBody Employee employee) {
         int id = this.id++;
-        Employee newEmployee = new Employee(1, employee.name(), employee.age(), employee.gender(), employee.salary());
+        Employee newEmployee = new Employee(id, employee.name(), employee.age(), employee.gender(), employee.salary());
         employees.add(newEmployee);
         return newEmployee;
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return employees.stream()
+                .filter(employee -> employee.id() == id)
+                .findFirst()
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
     }
 }
