70fd11681107421bb36ef5a55339ac1ea2b8b989
SULE <sule1@oocl.com>
2025-09-09 17:24:26 +0800

    fix： fix remove(index) in deleteEmployee

--- src/main/java/com/example/employee/EmployeeController.java
+++ src/main/java/com/example/employee/EmployeeController.java
@@ -74,12 +74,15 @@ public class EmployeeController {
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable int id) {
+        int index =0;
         for (int i = 0; i < employees.size(); i++) {
             Employee employee = employees.get(i);
             if (employee.id() == id) {
-               employees.remove(i);
+               index = i;
+               break;
             }
         }
+        employees.remove(index);
     }
 
 
